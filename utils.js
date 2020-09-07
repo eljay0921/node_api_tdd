2be64ddf232bb372e6942bb8710c878ca025fc97
jin <lj7812@gmail.com>
2020-09-07 21:19:57 +0900

    # 2.0 Preparing for testing with superTest

--- utils.js
+++
@@ -1,8 +0,0 @@
-const capitalize = (text) => {
-  return text.charAt(0).toUpperCase() + text.slice(1);
-};
-
-// export default capitalize;
-module.exports = {
-  capitalize, // capitalize : capitalize와 동일
-};
