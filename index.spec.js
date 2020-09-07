61d5725920d7de084e9e3a77af77bdc1d03aaa32
jin <lj7812@gmail.com>
2020-09-07 21:41:07 +0900

    # 3.0 TDD - GET users - SUCCESS

--- index.spec.js
+++ index.spec.js
@@ -1,14 +1,16 @@
-const app = require("./index");
 const request = require("supertest");
+const should = require("should");
+const app = require("./index");
 
 describe("GET /users는 ", () => {
-  it("다음 users를 나타낸다.", (done) => {
-    request(app)
-      .get("/users")
-      .end((err, res) => {
-        console.log(res.body);
-
-        done(); // 테스트가 끝났음을 알리는 콜백
-      });
+  describe("성공 시, ", () => {
+    it("user 객체를 담은 배열로 응답한다.", (done) => {
+      request(app)
+        .get("/users")
+        .end((err, res) => {
+          res.body.should.be.instanceOf(Array);
+          done(); // 테스트가 끝났음을 알리는 콜백 => 비동기로 동작하기 때문에 필요
+        });
+    });
   });
 });
