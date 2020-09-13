57e0dac68a42b08784d65755dfc71077afeb543e
jin <lj7812@gmail.com>
2020-09-13 16:11:11 +0900

    # 9.0 Fin
    옵션에 따라 db sync를 다르게 하여, 테스트할 때만 db를 강제로 sync하고, 서버를 구동할 때는 기존 파일을 유지하도록

--- bin/sync-db.js
+++ bin/sync-db.js
@@ -1,5 +1,8 @@
 const models = require("../models");
 
 module.exports = () => {
-  return models.sequelize.sync({ force: true });
+  const options = {
+    force: process.env.NODE_ENV === "test",
+  };
+  return models.sequelize.sync(options);
 };
